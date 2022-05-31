7a24581a343ff5762012152f78caec72394d3480
duongdo671999@gmail.com <duongdo671999>
2022-06-01 01:45:04 +0700

    deploy database

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 const app = express();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'postgres',
-    password: 'test',
-    database: 'smart-brain',
+    // host: '127.0.0.1',
+    // port: 5432,
+    // user: 'postgres',
+    // password: 'test',
+    // database: 'smart-brain',
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+    // ssl: {
+    //   rejectUnauthorized: false,
+    // },
   },
 });
 app.use(express.json());
